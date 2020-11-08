cf6e96155644fd12d8acd44ac5ee495a266297c2
JamesGoswell <James.goswell@sky.com>
2020-11-08 20:39:36 +0000

    small changes
    Remove stars, changed some text

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-function stars(n) {
-    // TODO Remove document.write()
-    let star_text = "";
-    let c = 0;
-
-    while (n > 0.5) {
-        star_text += '<i class="fas fa-star stars"></i>';
-        n--;
-        c++;
-    }
-    if (n == 0.5) {
-        star_text += '<i class="fas fa-star-half-alt stars"></i>';
-        c++;
-    }
-    while (c<5) {
-        star_text += '<i class="far fa-star stars"></i>';
-        c++;
-    }
-    return document.write(star_text);
-}
+// function stars(n) {
+//     // TODO Remove document.write()
+//     let star_text = "";
+//     let c = 0;
+//
+//     while (n > 0.5) {
+//         star_text += '<i class="fas fa-star stars"></i>';
+//         n--;
+//         c++;
+//     }
+//     if (n == 0.5) {
+//         star_text += '<i class="fas fa-star-half-alt stars"></i>';
+//         c++;
+//     }
+//     while (c<5) {
+//         star_text += '<i class="far fa-star stars"></i>';
+//         c++;
+//     }
+//     return document.write(star_text);
+// }
 function navtoggle(x) {
     x.classList.toggle("change");
     document.getElementById("nav").classList.toggle("navShow")
